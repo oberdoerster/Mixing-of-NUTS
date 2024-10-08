df341eb7824d793ec5e0cc6367dde21f0f54909c
oberdoerster <oberdoerster@uni-bonn.de>
2024-10-08 11:56:34 +0200

    Update figure5_plot.m

--- figure5_plot.m
+++ figure5_plot.m
@@ -11,9 +11,9 @@ x2_vec = load('x2_vec_10.mat').x2_vec;
 domain=(n_dim-10*sqrt(n_dim):1:n_dim+10*sqrt(n_dim));
 
 figure(1); hold on;
-histogram(x2_vec(2:3,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor','k');
+histogram(x2_vec(2:3,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor',[0.5,0.5,0.5]);
 plot(domain,chi2pdf(domain,n_dim),'k','LineWidth',5);
-plot(norm(init)^2,0,'k.','MarkerSize',80);
+plot(norm(init)^2,0,'k.','MarkerSize',60);
 set(gca,'fontsize', 25);
 xlim([n_dim-6*sqrt(n_dim) n_dim+6*sqrt(n_dim)]);
 ylim([0 0.003]);
@@ -28,9 +28,9 @@ x2_vec = load('x2_vec_11.mat').x2_vec;
 domain=(n_dim-10*sqrt(n_dim):1:n_dim+10*sqrt(n_dim));
 
 figure(2); hold on;
-histogram(x2_vec(26:51,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor','k');
+histogram(x2_vec(26:51,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor',[0.5,0.5,0.5]);
 plot(domain,chi2pdf(domain,n_dim),'k','LineWidth',5);
-plot(norm(init)^2,0,'k.','MarkerSize',80);
+plot(norm(init)^2,0,'k.','MarkerSize',60);
 set(gca,'fontsize', 25);
 xlim([n_dim-6*sqrt(n_dim) n_dim+6*sqrt(n_dim)]);
 ylim([0 0.003]);
@@ -45,9 +45,9 @@ x2_vec = load('x2_vec_5.mat').x2_vec;
 domain=(n_dim-10*sqrt(n_dim):1:n_dim+10*sqrt(n_dim));
 
 figure(3); hold on;
-histogram(x2_vec(636:661,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor','k');
+histogram(x2_vec(636:661,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor',[0.5,0.5,0.5]);
 plot(domain,chi2pdf(domain,n_dim),'k','LineWidth',5);
-plot(norm(init)^2,0,'k.','MarkerSize',80);
+plot(norm(init)^2,0,'k.','MarkerSize',60);
 set(gca,'fontsize', 25);
 xlim([n_dim-6*sqrt(n_dim) n_dim+6*sqrt(n_dim)]);
 ylim([0 0.003]);
@@ -62,9 +62,9 @@ x2_vec = load('x2_vec_99.mat').x2_vec;
 domain=(n_dim-10*sqrt(n_dim):1:n_dim+10*sqrt(n_dim));
 
 figure(4); hold on;
-histogram(x2_vec(1976:2001,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor','k');
+histogram(x2_vec(1976:2001,:),(n_dim-10*sqrt(n_dim):25:n_dim+10*sqrt(n_dim)),'Normalization','pdf','FaceColor',[0.5,0.5,0.5]);
 plot(domain,chi2pdf(domain,n_dim),'k','LineWidth',5);
-plot(norm(init)^2,0,'k.','MarkerSize',80);
+plot(norm(init)^2,0,'k.','MarkerSize',60);
 set(gca,'fontsize', 25);
 xlim([n_dim-6*sqrt(n_dim) n_dim+6*sqrt(n_dim)]);
 ylim([0 0.003]);
